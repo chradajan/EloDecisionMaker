e33c18862c397b3fe78171492c87048ce300ff1c
chradajan <chradajan@gmail.com>
2020-04-16 12:54:37 -0700

    Add options to toggle scores

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ k = 32
 class Ranking:
 
     itemsDict = {}
+    lastMatchup = []
+    longestItemName = 0
     left = ""
     right = ""
 
@@ -16,28 +18,56 @@ class Ranking:
         for i in reader:
             self.itemsDict[i.rstrip()] = 1500
 
+        for item in self.itemsDict.keys():
+            if len(item) > self.longestItemName:
+                self.longestItemName = len(item)
+
     def itemCount(self):
         return len(self.itemsDict)
 
-    def longestBreedName(self):
-        longestBreedName = 0
+    def getLongestItemName(self):
+        return self.longestItemName
 
-        for breed in self.itemsDict.keys():
-            if len(breed) > longestBreedName:
-                longestBreedName = len(breed)
+    def getItemsWithScores(self):
+        returnString = ""
 
-        return longestBreedName
-
-    def getString(self):
-        rankString = ""
-
-        for breed,score in sorted(self.itemsDict.items(), key = lambda x : (-x[1], x[0])):
-            rankString += "{brd:<{wid}} {scr}\n".format(brd=breed, wid = self.longestBreedName() + 5, scr = score)
+        for item,score in sorted(self.itemsDict.items(), key = lambda x : (-x[1], x[0])):
+            returnString += "{itm:<{wid}} {scr}\n".format(itm = item, wid = self.longestItemName + 5, scr = score)
         
-        return rankString.rstrip()
+        return returnString.rstrip()
+
+    def getItemsNoScores(self):
+        returnString = ""
+
+        for item,score in sorted(self.itemsDict.items(), key = lambda x : (-x[1], x[0])):
+            returnString += "{itm:<{wid}}\n".format(itm = item, wid = self.longestItemName)
+        
+        return returnString
+    
+    def getAlphabeticalItemsWithScores(self):
+        returnString = ""
+
+        for item in sorted(self.itemsDict.keys()):
+            returnString += "{itm:<{wid}} {scr}\n".format(itm = item, wid = self.longestItemName + 5, scr = self.itemsDict[item])
+
+        return returnString.rstrip()
+
+    def getAlphabeticalItemsNoScores(self):
+        returnString = ""
+
+        for item in sorted(self.itemsDict.keys()):
+            returnString += "{itm:<{wid}}\n".format(itm = item, wid = self.longestItemName)
+
+        return returnString
+
 
     def nextMatchup(self):
         matchup = sample(self.itemsDict.keys(), 2)
+        
+        while (matchup[0] in self.lastMatchup and matchup[1] in self.lastMatchup):
+            matchup = sample(self.itemsDict.keys(), 2)
+
+        self.lastMatchup = matchup
         self.left = matchup[0]
         self.right = matchup[1]
         return matchup
@@ -60,25 +90,62 @@ class GUI:
     def __init__(self, master, ranker):
         self.master = master
         self.ranker = ranker
+        self.hideScores = IntVar()
+        self.hideOrder = IntVar()
+        self.hideAll = IntVar()
         master.title("Elo Ranker")
 
+        #Left Button
         self.leftButton = Button(master, text = "", command = self.left)
         self.leftButton.grid(row = 0, column = 0, sticky = N + S + W + E)
 
+        #Right Button
         self.rightButton = Button(master, text = "", command = self.right)
         self.rightButton.grid(row = 0, column = 1, sticky = N + S + W + E)
 
-        self.ranking = Text(master, height = ranker.itemCount(), width = ranker.longestBreedName() + 15, state = DISABLED)
-        self.ranking.grid(row = 1, columnspan = 2, sticky = N + S + W + E)
+        #Text Box
+        self.ranking = Text(master, height = ranker.itemCount(), width = ranker.getLongestItemName() + 15, state = DISABLED)
+        # self.ranking.grid(row = 2, columnspan = 2, sticky = N + S + W + E)
+        self.ranking.grid(row = 1, column = 0, sticky = N + S + W + E)
         self.ranking.tag_configure("center", justify = "center")
         self.updateRanks()
 
+        #Menu
+        self.optionsFrame = Frame(master)
+        self.optionsFrame.grid(row = 1, column = 1, sticky = N + S + W + E)
+
+        self.optionsLabel = Label(self.optionsFrame, text = 'Options')
+        self.optionsLabel.pack()
+
+        self.hideScoresButton = Checkbutton(self.optionsFrame, text = 'Hide Scores', variable = self.hideScores, command = self.updateRanks)
+        self.hideOrderButton = Checkbutton(self.optionsFrame, text = 'Hide Order  ', variable = self.hideOrder, command = self.updateRanks)
+        self.hideAllButton = Checkbutton(self.optionsFrame, text = 'Hide List      ', variable = self.hideAll, command = self.hideAllFunction)
+        self.hideScoresButton.pack()
+        self.hideOrderButton.pack()
+        self.hideAllButton.pack()
+        
+
+        #Grid Configuration
         self.master.grid_columnconfigure(0, weight = 1, uniform = 'half')
         self.master.grid_columnconfigure(1, weight = 1, uniform = 'half')
         self.master.grid_rowconfigure(0, weight = 1)
         self.master.grid_rowconfigure(1, weight = 1)
 
         self.updateButtons()
+
+    def hideAllFunction(self):
+        if self.hideAll.get() == 1: #Button turned on
+            self.hideScoresButton.select()
+            self.hideScoresButton['state'] = DISABLED
+            self.hideOrderButton.select()
+            self.hideOrderButton['state'] = DISABLED
+        else:
+            self.hideScoresButton.deselect()
+            self.hideScoresButton['state'] = NORMAL
+            self.hideOrderButton.deselect()
+            self.hideOrderButton['state'] = NORMAL
+
+        self.updateRanks()
 
     def left(self):
         self.ranker.calculateResult("left")
@@ -98,7 +165,18 @@ class GUI:
     def updateRanks(self):
         self.ranking['state'] = NORMAL
         self.ranking.delete(1.0, END)
-        self.ranking.insert(END, ranker.getString())
+
+        if self.hideAll.get() == 1:
+            self.ranking.insert(END, "")
+        elif self.hideScores.get() == 1 and self.hideOrder.get() == 1:
+            self.ranking.insert(END, ranker.getAlphabeticalItemsNoScores())
+        elif self.hideScores.get() == 1:
+            self.ranking.insert(END, ranker.getItemsNoScores())
+        elif self.hideOrder.get() == 1:
+            self.ranking.insert(END, ranker.getAlphabeticalItemsWithScores())
+        else:    
+            self.ranking.insert(END, ranker.getItemsWithScores())
+
         self.ranking.tag_add("center", 1.0, END)
         self.ranking['state'] = DISABLED
 
